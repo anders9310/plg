ba9c6930ce907a7a9d252300276e57994d4d64fb
Anders <s121919@student.dtu.dk>
2017-09-13 18:02:58 +0200

    Process generation: Fix ParameterConfiguration used

--- libPlg/src/plg/generator/process/ObligationsProcessGenerator.java
+++ libPlg/src/plg/generator/process/ObligationsProcessGenerator.java
@@ -3,7 +3,7 @@ package plg.generator.process;
 import plg.model.Process;
 
 public class ObligationsProcessGenerator extends ProcessGenerator{
-    public ObligationsProcessGenerator(Process process, Plg2RandomizationConfiguration parameters){
+    public ObligationsProcessGenerator(Process process, ParameterRandomizationConfiguration parameters){
         super(process, parameters);
     }
 
