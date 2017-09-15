88e7c83c1ee7bb5d71639395bd00384a47b97527
Anders <s121919@student.dtu.dk>
2017-09-15 15:54:46 +0200

    Generation of many processes

--- libPlg/src/plg/test/TestProcessGeneration.java
+++ libPlg/src/plg/test/TestProcessGeneration.java
@@ -1,16 +1,26 @@
 package plg.test;
 
 import plg.generator.process.*;
+import plg.io.exporter.BPMNExporter;
 import plg.model.Process;
 
 public class TestProcessGeneration {
 
+    private static final int NUM_GENERATED_MODELS = 500;
+
     public static void main(String[] args) throws Exception {
-        int maxValue = 1000000;
-        // process  randomization
-        Process p =new Process("test" );
-        ObligationsProcessGenerator.randomizeProcess(p, ParameterRandomizationConfiguration.BASIC_VALUES);
-        //Plg2ProcessGenerator.randomizeProcess(p, new Plg2RandomizationConfiguration(maxValue,maxValue,0.1,0.2,0.1,0.7,0.3,0.3,maxValue,0.1));
-        System.out.print(p.toString());
+        BPMNExporter e = new BPMNExporter();
+        String basePath = "D:\\Users\\Anders\\Documents\\MasterThesis\\PlgModels";
+        String baseFileName = "bpmnmodel_";
+        String bpmnExtension = ".bpmn";
+        for(int i = 0; i< NUM_GENERATED_MODELS; i++){
+            Process p =new Process("test" );
+            ObligationsProcessGenerator.randomizeProcess(p, new ParameterRandomizationConfiguration(10, 5));
+
+            String modelNumber = String.valueOf(i);
+            String path = basePath + "\\" + baseFileName + modelNumber + bpmnExtension;
+            e.exportModel(p, path);
+        }
+
     }
 }
